caab762c5d92235a32c54caf5a2ae09bc5c9a7b2
Heather <hkim@kipsu.com>
2023-06-20 15:24:02 -0500

    hide api key

--- app/src/components/Demo.tsx
+++ app/src/components/Demo.tsx
@@ -1,12 +1,25 @@
 import Timer from './Timer';
+import Giphy from './Giphy';
+import { useEffect, useState } from 'react';
+import { Navigate, useNavigate } from '@tanstack/react-router';
 
 const Demo = () => {
-  return (
-    <div>
-      <h1>Play demo game</h1>
-      <Timer />
-    </div>
-  );
+  const number = Math.floor(Math.random() * 1000);
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setRedirect(true);
+    }, 3000);
+
+    return () => clearTimeout(timeOut);
+  }, []);
+
+  if (redirect) {
+    return <Navigate to="/timer" />;
+  }
+
+  return <>{number}</>;
 };
 
 export default Demo;
