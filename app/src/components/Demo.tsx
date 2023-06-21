f1b2b0effe3413ad3231ac3ee8c411195df37911
Heather <hkim@kipsu.com>
2023-06-21 08:06:03 -0500

    added app/.env to gitignore

--- app/src/components/Demo.tsx
+++ app/src/components/Demo.tsx
@@ -4,22 +4,24 @@ import { useEffect, useState } from 'react';
 import { Navigate, useNavigate } from '@tanstack/react-router';
 
 const Demo = () => {
-  const number = Math.floor(Math.random() * 1000);
-  const [redirect, setRedirect] = useState(false);
+  // const number = Math.floor(Math.random() * 1000);
+  // const [redirect, setRedirect] = useState(false);
+  //
+  // useEffect(() => {
+  //   const timeOut = setTimeout(() => {
+  //     setRedirect(true);
+  //   }, 3000);
+  //
+  //   return () => clearTimeout(timeOut);
+  // }, []);
+  //
+  // if (redirect) {
+  //   return <Navigate to="/timer" />;
+  // }
+  //
+  // return <>{number}</>;
 
-  useEffect(() => {
-    const timeOut = setTimeout(() => {
-      setRedirect(true);
-    }, 3000);
-
-    return () => clearTimeout(timeOut);
-  }, []);
-
-  if (redirect) {
-    return <Navigate to="/timer" />;
-  }
-
-  return <>{number}</>;
+  return <Giphy />;
 };
 
 export default Demo;
