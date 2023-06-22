840fb0bfae1de7b3be7e362a841011d65b5aeb53
Heather <hkim@kipsu.com>
2023-06-21 20:57:10 -0500

    created a button that can generate an array of non-repeating random numbers

--- app/src/components/Demo.tsx
+++ app/src/components/Demo.tsx
@@ -5,6 +5,26 @@ import { Navigate, useNavigate } from '@tanstack/react-router';
 import RandomFacts from './RandomFacts';
 
 const Demo = () => {
+  const [number, setNumber] = useState(0);
+  const [numbers, setNumbers] = useState([]);
+
+  const handleClick = () => {
+    const n = 3;
+    const array = [];
+    do {
+      const randomNumber = Math.floor(10000 + Math.random() * 89999);
+
+      if (!array.includes(randomNumber)) {
+        array.push(randomNumber);
+      }
+    } while (array.length < n);
+    setNumbers(array);
+  };
+
+  const randomNumbers = numbers.map((item, index) => (
+    <h1 key={index}>{item}</h1>
+  ));
+
   // const number = Math.floor(Math.random() * 1000);
   // const [redirect, setRedirect] = useState(false);
   //
@@ -19,10 +39,13 @@ const Demo = () => {
   // if (redirect) {
   //   return <Navigate to="/timer" />;
   // }
-  //
-  // return <>{number}</>;
 
-  return <RandomFacts />;
+  return (
+    <div>
+      <button onClick={handleClick}>START</button>
+      <div>{randomNumbers}</div>
+    </div>
+  );
 };
 
 export default Demo;
