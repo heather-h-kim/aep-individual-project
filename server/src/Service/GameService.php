641f64fa37ccee85d8c08b0fda3cd43aba9c642e
Heather <hkim@kipsu.com>
2023-06-28 17:03:21 -0500

    wrote createGameLevelsRounds function in GameService

--- server/src/Service/GameService.php
+++ server/src/Service/GameService.php
@@ -16,22 +16,24 @@ class GameService
     private SeasonRepository $seasonRepository;
     private UserRepository $userRepository;
     private LevelService $levelService;
+    private RoundService $roundService;
 
     /**
      * @param GameRepository $gameRepository
      */
-    public function __construct(GameRepository $gameRepository, SeasonRepository $seasonRepository, UserRepository $userRepository, LevelService $levelService)
+    public function __construct(GameRepository $gameRepository, SeasonRepository $seasonRepository, UserRepository $userRepository, LevelService $levelService, RoundService $roundService)
     {
         $this->gameRepository = $gameRepository;
         $this->seasonRepository = $seasonRepository;
         $this->userRepository = $userRepository;
         $this->levelService = $levelService;
+        $this->roundService = $roundService;
     }
 
     /**
      * @throws NonUniqueResultException
      */
-    public function createGame(CreateGameRoundDto $createGameRoundDto): int
+    public function createGame(CreateGameRoundDto $createGameRoundDto): Game
     {
         $newGame = new Game();
         $user_id = $createGameRoundDto->getUserId();
@@ -49,11 +51,24 @@ class GameService
         // $levels = an array of LevelDtos
         $levels = $createGameRoundDto->getLevelsRounds();
 
+
+
         //$level = LevelDto
-        foreach( $levels as $level){
-           $createdLevel = $this->levelService->createLevel($level);
+        foreach( $levels as $levelDto){
+           $createdLevel = $this->levelService->createLevel($levelDto);
            $createdLevel->setGameId($game_id);
+
+           $level_id = $createdLevel->getId();
+
+           //$rounds = an array of RoundDtos
+           $rounds = $levelDto->getRounds();
+           foreach( $rounds as $roundDto){
+               $createdRound = $this->roundService->createRound($roundDto);
+               $createdRound->setLevelId($level_id);
+           }
         }
+
+        return $newGame;
 
 
     }
