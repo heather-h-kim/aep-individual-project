e84ec264a13a3fbd3609e957c17cf22005e0bf63
Heather <hkim@kipsu.com>
2023-07-17 09:38:09 -0500

    added search by username functionality

--- app/src/components/Rankings.tsx
+++ app/src/components/Rankings.tsx
@@ -9,6 +9,7 @@ import Pagination from './Pagination';
 const Rankings = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [rankingsPerPage, setRankingsPerPage] = useState(5);
+  const [search, setSearch] = useState('');
 
   // global states for the dropdown menu and getting rankings
   const { seasons, currentSeasonId, updateCurrentSeasonId } = useSeasonStore(
@@ -56,29 +57,47 @@ const Rankings = () => {
   //Change rankings page
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
-  return (
-    <div>
-      <label
-        htmlFor="countries"
-        className="mb-2 block text-sm font-medium text-gray-900"
-      >
-        Select season
-      </label>
-      <select
-        id="countries"
-        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-        onChange={handleSelect}
-      >
-        <option defaultValue={currentSeasonId}>Season {currentSeasonId}</option>
-        {seasons.map(season => {
-          return (
-            <option key={season.seasonId} value={season.seasonId}>
-              Season {season.seasonId}
-            </option>
-          );
-        })}
-      </select>
+  console.log(search);
+
+  if (search == '') {
+    return (
       <div>
+        <div className="flex flex-row justify-evenly">
+          <div>
+            <label
+              htmlFor="seasons"
+              className="mb-2 block text-sm font-medium text-gray-900"
+            >
+              Select season
+            </label>
+            <select
+              id="seasons"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              onChange={handleSelect}
+            >
+              <option defaultValue={currentSeasonId}>
+                Season {currentSeasonId}
+              </option>
+              {seasons.map(season => {
+                return (
+                  <option key={season.seasonId} value={season.seasonId}>
+                    Season {season.seasonId}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <form>
+            <label>
+              Search by Username:
+              <input
+                type="text"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+              />
+            </label>
+          </form>
+        </div>
         <table className="table-auto">
           <thead>
             <tr>
@@ -105,7 +124,76 @@ const Rankings = () => {
           paginate={paginate}
         />
       </div>
-    </div>
-  );
+    );
+  }
+  if (search !== '') {
+    return (
+      <div>
+        <div className="flex flex-row justify-evenly">
+          <div>
+            <label
+              htmlFor="seasons"
+              className="mb-2 block text-sm font-medium text-gray-900"
+            >
+              Select season
+            </label>
+            <select
+              id="seasons"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              onChange={handleSelect}
+            >
+              <option defaultValue={currentSeasonId}>
+                Season {currentSeasonId}
+              </option>
+              {seasons.map(season => {
+                return (
+                  <option key={season.seasonId} value={season.seasonId}>
+                    Season {season.seasonId}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <form className="flex flex-row items-center">
+            <label>
+              Search by Username:
+              <input
+                type="text"
+                value={search}
+                onChange={e => setSearch(e.target.value)}
+              />
+            </label>
+            <button onClick={() => setSearch('')}>back</button>
+          </form>
+        </div>
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Username</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rankings
+              .filter(ranking => {
+                if (search.toLowerCase() !== '') {
+                  return ranking.userName.toLowerCase().includes(search);
+                }
+              })
+              .map(ranking => {
+                return (
+                  <tr key={ranking.userName}>
+                    <td>{ranking.rank}</td>
+                    <td>{ranking.userName}</td>
+                    <td>{ranking.topScore}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 };
 export default Rankings;
