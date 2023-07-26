ba233f18403f46d53a73b989f60f4376e678174b
Heather <hkim@kipsu.com>
2023-07-26 10:56:57 -0500

    added exceptions to throw in the user service

--- server/src/Controller/UserController.php
+++ server/src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Dto\Incoming\UpdateUserDto;
 use App\Exception\InvalidRequestDataException;
 use App\Serialization\SerializationService;
 use App\Service\UserService;
+use Exception;
 use JsonException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,19 +26,25 @@ class UserController extends ApiController
     }
 
     /**
-     * @throws InvalidRequestDataException
-     * @throws JsonException
+     * @throws Exception
      */
 
     #[Route('api/user', methods: ['POST'])]
     public function createUser(Request $request): Response
     {
-        /**
-         * @var CreateUserDto $dto
-         */
-        $dto = $this->getValidatedDto($request, CreateUserDto::class);
-        return $this->json($this->userService->createUser($dto));
+        try {
+            /**
+             * @var CreateUserDto $incomingDto
+             */
+            $incomingDto = $this->getValidatedDto($request, CreateUserDto::class);
+            $outgoingDto= $this->userService->createUser($incomingDto);
+        } catch (Exception $e){
+            $this->json($e->getMessage());
+        }
+
+        return $this->json($outgoingDto);
     }
+
 
     #[Route('api/users', methods: ['GET'])]
     public function getUsers(): Response
@@ -58,26 +65,40 @@ class UserController extends ApiController
     }
 
 
-
     /**
-     * @throws JsonException
-     * @throws InvalidRequestDataException
+     * @throws Exception
      */
+
     #[Route('api/user', methods: ['PATCH', 'PUT'])]
     public function updateUser(Request $request): Response
     {
-        /**
-         * @var UpdateUserDto $dto
-         */
-        $dto = $this->getValidatedDto($request, UpdateUserDto::class);
-//        return $this->json($dto);
-        return $this->json($this->userService->updateUser($dto));
+        try{
+            /**
+            * @var UpdateUserDto $incomingDto
+            */
+        $incomingDto = $this->getValidatedDto($request, UpdateUserDto::class);
+        $outgoingDto = $this->userService->updateUser($incomingDto);
+
+        } catch (Exception $e) {
+            return $this->json($e->getMessage());
+        }
+
+        return $this->json($outgoingDto);
     }
+
+    /**
+     * @throws Exception
+     */
 
     #[Route('api/user/{id}', methods: ['DELETE'])]
     public function deleteUser(int $id): Response
     {
-        return $this->json($this->userService->deleteUser($id));
+        try{
+            $result = $this->userService->deleteUser($id);
+        } catch (Exception $e) {
+            return $this->json($e->getMessage());
+        }
+        return $this->json($result);
     }
 
 }
